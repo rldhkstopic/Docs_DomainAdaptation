c8bbadbb9f3d2f25564a5ec565722b973c3926d7
rldhkstopic <rldhks6899@naver.com>
2023-10-20 20:28:43 +0900

    + domain loss : 0.753204

--- DANN_v2/domain.py
+++ DANN_v2/domain.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optims
 import torch.nn.functional as F
+from torch.nn import BCEWithLogitsLoss
 
 from tqdm import tqdm
 import torchvision.models as models
@@ -30,9 +31,9 @@ class Network():
             
         self.D = Discriminator().to(self.device)                              # Discriminator
         self.E = nn.Sequential(*layout[:-1]).to(self.device)                  # Encoder
-        self.C = nn.Sequential(nn.Linear(512, 100, bias=True),
+        self.C = nn.Sequential(nn.Linear(512, 100, bias=True),                # Classifier
                                 nn.ReLU(), 
-                                nn.Linear(100, 10)).to(self.device)           # Classifier
+                                nn.Linear(100, 10)).to(self.device)           
         
         self.CELoss = nn.CrossEntropyLoss().to(self.device)
         self.BCELoss = nn.BCELoss().to(self.device)
@@ -62,7 +63,7 @@ class Network():
         self.model.train()
         optim = optims.Adam(
             list(self.C.parameters()) + 
-            list(self.E.parameters()), lr=0.01)
+            list(self.E.parameters()), lr=0.001)
         
         update_interval = 10
         print(">> Only-Source Domain Training Start")    
@@ -88,7 +89,7 @@ class Network():
                 if i % update_interval == 0 :
                     bar.set_description("Epoch: {}/{} - Class Loss: {:.6f}".format(epoch+1, epochs, classification_loss.item()), refresh=True)
         
-            if (epoch + 1) % 2 == 0:
+            if (epoch + 1) % 10 == 0:
                 self._test_source()
         
         self.writer.close()
@@ -114,57 +115,64 @@ class Network():
                 
         print(f'Source Domain Accuracy: {100 * correct_source / total_source:.2f}%')
         
+    
     def _train_dann(self, epochs):
         optim = optims.Adam(
             list(self.C.parameters()) + 
             list(self.E.parameters()) + 
-            list(self.D.parameters()), lr=0.01)
+            list(self.D.parameters()), lr=0.001)
         
         update_interval = 50
+        self.BCELoss = BCEWithLogitsLoss()
         print(">> Domain-Adversarial Training Start")
         for epoch in range(epochs):
-            print('Epoch : {}'.format(epoch))
             start_steps = epoch * len(self.dataset_source)
             total_steps = epochs * len(self.dataset_target)
             bar = tqdm(enumerate(zip(self.dataset_source, self.dataset_target)), total=min(len(self.dataset_source), len(self.dataset_target)))
-            for i, (source_data, target_data) in bar:
-                source, source_labels = source_data
-                target, _ = target_data
+            for i, (data_s, data_t) in bar:
+                source_data, source_labels = data_s
+                source_data = torch.cat((source_data, source_data, source_data), 1) # MNIST convert to 3 channel
+                target_data, target_labels = data_t
                 
                 p = float(i + start_steps) / total_steps
                 alpha = 2. / (1. + np.exp(-10 * p)) - 1
                 
-                source_data = torch.cat((source, source, source), 1) # MNIST convert to 3 channel
                 source_data, source_labels = source_data.to(self.device), source_labels.to(self.device)
-                target_data, target_labels = target.to(self.device), target.to(self.device)
+                target_data, target_labels = target_data.to(self.device), target_labels.to(self.device)
                 total_data = torch.cat((source_data, target_data), 0)
 
                 optim = optim_scheduler(optim=optim, p=p)
                 optim.zero_grad()
                 
                 source_feature = self.E(source_data) # source_feature : [32, 512, 1, 1]
-                total_feature = self.E(total_data)   # total_feature : [64, 512, 1, 1]
-                total_feature = total_feature.view(total_feature.size(0), -1) # total_feature : [64, 512]
-                
                 cls_predicted = self.C(source_feature.view(source_feature.size(0), -1)) 
                 cls_loss = self.CELoss(cls_predicted, source_labels)
                 
+                total_feature = self.E(total_data)   # total_feature : [64, 512, 1, 1]
+                total_feature = total_feature.view(total_feature.size(0), -1) # total_feature : [64, 512]
+                
                 domain_predicted = self.D(total_feature, alpha)
-                domain_source_labels = torch.zeros(source_labels.shape[0]).type(torch.LongTensor)
-                domain_target_labels = torch.ones(target_labels.shape[0]).type(torch.LongTensor)
+                domain_source_labels = torch.zeros(source_labels.shape[0], dtype=torch.long, device=self.device)
+                domain_target_labels = torch.ones(target_labels.shape[0], dtype=torch.long, device=self.device)
+                
                 domain_total_labels = torch.cat((domain_source_labels, domain_target_labels), 0)
-                domain_total_labels_onehot = F.one_hot(domain_total_labels, num_classes=2).type(torch.FloatTensor).to(self.device)
+                domain_total_labels_onehot = F.one_hot(domain_total_labels, num_classes=2).float()
+                
                 domain_loss = self.BCELoss(domain_predicted, domain_total_labels_onehot)
                 
                 total_loss = cls_loss + domain_loss
                 total_loss.backward()
                 optim.step()
                 
+                self.writer.add_scalar('Train/Loss/Classification', cls_loss.item(), epoch * len(self.dataset_source) + i)
+                self.writer.add_scalar('Train/Loss/Domain', domain_loss.item(), epoch * len(self.dataset_source) + i)
+                self.writer.add_scalar('Train/Loss/Total', total_loss.item(), epoch * len(self.dataset_source) + i)
+            
                 if i % update_interval == 0 :
                     bar.set_description("Epoch: {}/{} - Class Loss: {:.6f} - Domain Loss: {:.6f} - Total Loss: {:.6f}".format(epoch+1, epochs, cls_loss.item(), domain_loss.item(), total_loss.item()), refresh=True)
 
-                if (epoch + 1) % 10 == 0:
-                    self._test_dann()
+            if (epoch + 1) % 10 == 0:
+                self._test_dann()
         
         modelSave(self.E, self.C, self.D, '_dann', isDann=True)
         
@@ -175,42 +183,46 @@ class Network():
         correct_source, total_source = 0, 0
         correct_target, total_target = 0, 0
         with torch.no_grad():          
-            for images, labels in self.dataset_source:
-                images, labels = images.to(self.device), labels.to(self.device)
+            for data_s, labels_s in self.dataset_source:
+                source_data = torch.cat((data_s, data_s, data_s), 1) if data_s.shape[1] == 1 else data_s
+                source_data, source_labels = source_data.to(self.device), labels_s.to(self.device)
                 
-                features = self.E(images) 
+                features = self.E(source_data) 
                 features = features.view(features.size(0), -1) 
                 outputs = self.C(features) 
                 _, predicted = outputs.max(1)
                 
-                total_source += labels.size(0)
-                correct_source += predicted.eq(labels).sum().item()
+                total_source += source_labels.size(0)
+                correct_source += predicted.eq(source_labels).sum().item()
                 
-                print(f'> | Source Domain Accuracy: {100 * correct_source / total_source:.2f}%')
                 
-            for images, labels in self.dataset_target:
-                images, labels = images.to(self.device), labels.to(self.device)
+            for data_t, labels_t in self.dataset_target:
+                target_data = torch.cat((data_t, data_t, data_t), 1) if data_t.shape[1] == 1 else data_t
+                target_data, target_labels = data_t.to(self.device), labels_t.to(self.device)
                 
-                features = self.E(images) 
+                features = self.E(target_data) 
                 features = features.view(features.size(0), -1) 
                 outputs = self.C(features) 
                 _, predicted = outputs.max(1)
                 
-                total_target += labels.size(0)
-                correct_target += predicted.eq(labels).sum().item()
+                total_target += target_labels.size(0)
+                correct_target += predicted.eq(target_labels).sum().item()
 
-                print(f'> | Target Domain Accuracy: {100 * correct_target / total_target:.2f}%')
-
-            print(f'> | Total Domain Accuracy: {100 * (correct_source + correct_target) / (total_source + total_target):.2f}%')
-
+        print(f'> | Source Domain Accuracy: {100 * correct_source / total_source:.2f}%')
+        print(f'> | Target Domain Accuracy: {100 * correct_target / total_target:.2f}%')
+        print(f'> | Total Domain Accuracy: {100 * (correct_source + correct_target) / (total_source + total_target):.2f}%')
 
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.discriminator = nn.Sequential(
-            nn.Linear(in_features=512, out_features=100),
-            nn.ReLU(),
-            nn.Linear(in_features=100, out_features=2)
+                nn.Linear(in_features=512, out_features=256),
+                nn.ReLU(),
+                nn.Dropout(0.5),  # optional, for regularization
+                nn.Linear(in_features=256, out_features=100),
+                nn.ReLU(),
+                nn.Dropout(0.5),  # optional, for regularization
+                nn.Linear(in_features=100, out_features=2)
         )
     
     def forward(self, input_feature, alpha):
@@ -227,8 +239,7 @@ class ReverseLayerF(torch.autograd.Function):
     
     @staticmethod
     def backward(ctx, grad_output):
-        return grad_output.neg() * ctx.alpha, None
-    
+        return grad_output.neg() * ctx.alpha, None   
     
 
 
--- DANN_v2/main.py
+++ DANN_v2/main.py
@@ -1,15 +1,19 @@
 from domain import Network
 from dataset import MNIST_loaders
 
-batch_size = 256
+batch_size = 64
 num_workers = 4
 
 def main():
     source, target = MNIST_loaders(batch_size, num_workers, type='train')
     
     net = Network(source, target)
+    net._load_model()
+    net._test_source()
+    
     if not net._load_model():
-        net._train_source(epochs=10)
+        net._train_source(epochs=20)
+    
     net._train_dann(epochs=100)   
 
 if __name__ == '__main__':
